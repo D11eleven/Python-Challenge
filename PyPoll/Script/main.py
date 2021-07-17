760ad02b17186fb3c256ce89f55c23665816db48
Darrell S Horich <d11eleven@gmail.com>
2021-07-17 00:26:11 -0500

    Comment Sections for each task in Both Projects

--- PyPoll/Script/main.py
+++ PyPoll/Script/main.py
@@ -3,8 +3,8 @@
 import os
 import csv
 
+csvpath = os.path.join("..","Resources","election_data.csv")
 
-csvpath = os.path.join( '..', 'Resources', 'election_data.csv')
 # # '..', 'Resources',
 
 with open(csvpath) as csvfile:
@@ -17,3 +17,60 @@ with open(csvpath) as csvfile:
 
      for row in csvreader:
          print(row)
+
+
+# Total Number of Months Included in the DataSet
+# **  need beginning date and ending date; count, find difference(inclusive)
+
+
+
+# Net Total Amount of "Profit/Losses"("PnL") over Entire Period
+# **Sum PnL column
+
+
+
+
+
+# Calculate the Changes in "PnL" over the Entire Period, 
+# then Find the Average of those Changes
+# ** Take Difference of Row I+1 and I and store as separate value for each row I; 
+# ** then add those values and divide by number of entries
+
+
+
+
+
+
+# The Greatest Increase in Profits (Date and Amount) over the Entire Period
+# **Find MAX in PnL and match its date
+
+
+# The Greatest Decrease in Profits (Date and Amount) over the Entire Period
+# **Find MAX in PnL and match its date
+
+
+
+# Output to Txt File
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TXT File Format
+# Financial Analysis
+# Total Months: 86
+# Total: $38382578
+# Average Change: $-2315.12
+# Greatest Increase in Profits: Feb-2012 ($1926159)
+# Greatest Decrease in Profits: Sep-2013 ($-2196167)
--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 
 HW#3 - Py Me Up, Charlie
 
-Code started on both projects -  will concentrate on PyBank from this point on til completion
-
-PyBank -  Import OS, CSV ;  Open/Read budget_data.csv - completed 7/16 AM
-
-PyPoll - Import OS, CSV ;  Open/Read budget_data.csv - completed 7/16 PM
+Organized code with section comments for each task . Tidywork 
