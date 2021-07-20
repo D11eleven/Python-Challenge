f26cc47f537a7256c271ac167d4bea5a3666b360
Darrell S Horich <d11eleven@gmail.com>
2021-07-19 20:29:28 -0500

    First Run of Code - Partially Correct

--- PyBank/Script/main.py
+++ PyBank/Script/main.py
@@ -7,35 +7,50 @@ import csv
 csvpath = os.path.join("..","Resources","budget_data.csv")
 # # '..', 'Resources',
 
-with open (csvpath, newline='', encoding='utf-8') as csvfile:
-     csvreader = csv.reader(csvfile, delimiter=',')
-# add encode and new line 2-12
-     print(csvreader)
-
-#read header row first 2-8,3-1
-     csv_header = next(csvreader)
-     print(f"CSV Header: {csv_header}")
-#read each row of data after header 2-8
-     for row in csvreader:
-         print(row)
 
 
 Total_Months = 0
 PnL_Total = 0 
 
 #Average_Change
-# val1 -val2  = change
+# val2 -val1  = change
 PnL_Value1 = 0
 Total_PnL_Change = 0
 
 #MAX_Increase_PNL
 #MIN_Increase_PNL 
-Date[]
-PnL[]
-PnL_Change[]  
+date = []
+PnL = []
+PnL_Change = []  
+
+with open (csvpath, newline='', encoding='utf-8') as csvfile:
+     csvreader = csv.reader(csvfile, delimiter=',')
+# add encode and new line 2-12
+    # print(csvreader)
+
+#read header row first 2-8,3-1
+     csv_header = next(csvreader)
+     print(f"CSV Header: {csv_header}")
+#read each row of data after header 2-8
+     for row in csvreader:
+         #print(row)
+
+
+#with open(budget_csvpath) as budget_csvfile:
+    #reads each row in the csv as a list
+ #   csvreader = csv.reader(budget_csvfile)
+    #skip the header
+  #  header = next(csvreader)
+
+
 
 # extract and save data from each row into seperate lists 
 #  Date[] & Profit/Losses (PnL[])  2-12 udemy     
+
+
+# open the data file and start analysis
+
+
 
 # Total Number of Months Included in the DataSet
 # **  need beginning date and ending date; count, find difference(inclusive)
@@ -44,15 +59,15 @@ PnL_Change[]
 #Total_Months = Total_Months + 1
 # initiate Total_Months = 0
 # create list of months ; use len for number of entries >>>>  number of months 
-Date.append(row[0])
-Total_Months =len(Date)
+          date.append(row[0])
+          Total_Months =len(date)
 #Put months in list 
 
 
 # Net Total Amount of "Profit/Losses"("PnL") over Entire Period
 # **Sum PnL column
-PnL.append(row[1])
-PnL_Total = PnL_Total + ???????? (row[1])
+          PnL.append(row[1])
+          PnL_Total = PnL_Total + int(row[1])
 
 
 # Calculate the Changes in "PnL" over the Entire Period, 
@@ -60,33 +75,37 @@ PnL_Total = PnL_Total + ???????? (row[1])
 # ** Take Difference of Row I+1 and I and store as separate value for each row I; 
 # ** then add those values and divide by number of entries  ???
 #  need clarification 
-PnL_Value2 = int(row[1])
-PnL_Change = PnL_Value2 - PnL_Value1
-#Average_Change = ()
+          PnL_Value2 = int(row[1])
+          change = PnL_Value2 - PnL_Value1
+          PnL_Change.append(change)
+
+          Total_PnL_Change = Total_PnL_Change + change
+          Average_Change = (Total_PnL_Change / Total_Months)
+          PnL_Value1 = PnL_Value2
 
 
 # The Greatest Increase in Profits (Date and Amount) over the Entire Period
 # **Find MAX in PnL changes and match its date
 
-MAX_Increase_PNL = max(PnL_Change)
+          MAX_Increase_PNL = max(PnL_Change)
 
 
 # The Greatest Decrease in Profits (Date and Amount) over the Entire Period
 # **Find MIN in PnL changes and match its date
 
-MIN_Increase_PNL = min(PnL_Change)
+          MIN_Increase_PNL = min(PnL_Change)
 
 
 
 # Print Results 
 
-#print ("    Financial Analysis")
-#print ("-------------------------------")
-print ("Total_Months: Total_Months")
-print ("Total: PnL_Total")
-print ("Average Change: $ xxxxxxxxxxxx")
-print ("Greatest Increase in Profits: (xxxxxxxx))
-print ("Greatest Decrease in Profits: (xxxxxxxxxx))
+print (f"    Financial Analysis")
+print (f"-------------------------------")
+print (f"Total_Months: {Total_Months}")
+print (f"Total: {PnL_Total}")
+print (f"Average Change: {Average_Change}")
+print (f"Greatest Increase in Profits: {MAX_Increase_PNL}")
+print (f"Greatest Decrease in Profits: {MIN_Increase_PNL}")
 
 
 
@@ -94,6 +113,15 @@ print ("Greatest Decrease in Profits: (xxxxxxxxxx))
 
 #output_path = os.path.join("..", "Output", "Analysis.txt")
 #with open(output_path, "w", newline='') as txtfile
+
+
+#output ("    Financial Analysis")
+#prvvvvnt ("-------------------------------")
+#prvvvint ("Total_Months: xxxx")
+#pvvvrint ("Total: xxxxxxxxxx")
+#prvvvint ("Average Change: $ xxxxxxxxxxxx")
+#prvvvint ("Greatest Increase in Profits: (xxxxxxxx))
+#pvvvvrnt ("Greatest Decrease in Profits: (xxxxxxxxxx))
 
 
 
@@ -107,20 +135,6 @@ print ("Greatest Decrease in Profits: (xxxxxxxxxx))
     # Write the second row
    # csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
 #from 2-10
-
-
-
-
-
-#output ("    Financial Analysis")
-#prvvvvnt ("-------------------------------")
-#prvvvint ("Total_Months: xxxx")
-#pvvvrint ("Total: xxxxxxxxxx")
-#prvvvint ("Average Change: $ xxxxxxxxxxxx")
-#prvvvint ("Greatest Increase in Profits: (xxxxxxxx))
-#pvvvvrnt ("Greatest Decrease in Profits: (xxxxxxxxxx))
-
-
 
 
 
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 HW#3 - Py Me Up, Charlie
 
-Work on different Sections of PyBank - nothing complete
+Work on different Sections of PyBank - 
+1st Run prints OK ; Partially Correct Calculations; Avg Change Incorrect; 
+No Months Associated with MAX/MIN Changes
